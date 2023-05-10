6d80319de387f452a1c777a69f8669def4b58fd2
jakesungjaekim <103394638+jakesungjaekim@users.noreply.github.com>
2023-05-10 20:19:03 +0900

    Add: section3 type annotation & inference

--- section3-annotation-inference/annotations/variables.ts
+++ section3-annotation-inference/annotations/variables.ts
@@ -26,3 +26,41 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20
 }
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+}
+
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number} = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line
+// and initialize it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // let foundWord = false;
+
+for( let i = 0; i < words.length; i++) {
+  if(words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) variable whose cannot be inferred cor
+
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false; 
+
+for( let i = 0; i < numbers.length; i++) {
+  if(numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
+
+// 
